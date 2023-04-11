f15ed604244c895030f8c55ec6ca6b7d1989dec8
abackhoff <129203814+abackhoff@users.noreply.github.com>
2023-04-11 09:43:16 -0400

    Delete email_utils.py

--- email_utils.py
+++
@@ -1,9 +0,0 @@
-from flask_mail import Mail, Message
-
-def send_email(recipient, subject, body):
-    mail = Mail()
-    message = Message(subject=subject, sender='your-email-address', recipients=['albertobackhoff@gmail.com'])
-    message.body = body
-    mail.send(message)
-
-    

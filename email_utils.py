417aa5ce925b4f990f9f3119acf271beab973a05
abackhoff <129203814+abackhoff@users.noreply.github.com>
2023-04-10 20:00:23 -0400

    Update email_utils.py

--- email_utils.py
+++ email_utils.py
@@ -2,7 +2,7 @@ from flask_mail import Mail, Message
 
 def send_email(recipient, subject, body):
     mail = Mail()
-    message = Message(subject=subject, sender='your-email-address', recipients=[recipient])
+    message = Message(subject=subject, sender='your-email-address', recipients=['albertobackhoff@gmail.com'])
     message.body = body
     mail.send(message)
 
